baed3e31a970bd6c93bba8919b9cf887e31e283e
Edwin Zhang <edwinzhang64@gmail.com>
2018-03-12 22:30:48 -0400

    Fix styles and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import Ionicon from 'react-ionicons'
 
 class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
-       <a href='https://github.com/edwinzhng' target='_blank' className='git'>
-         <i className='icon git ion-social-github transition'></i>
+       <a href='https://github.com/edwinzhng' target='_blank'>
+        <Ionicon className="ionicon git" icon="logo-github" color="#fff" fontSize="40px"/>
        </a>
-       <a href='https://linkedin.com/in/edwin-zhang/' target='_blank' className='linkedin'>
-         <i className='icon ion-social-linkedin transition'></i>
+       <a href='https://linkedin.com/in/edwin-zhang/' target='_blank'>
+        <Ionicon className="ionicon linkedin" icon="logo-linkedin" color="#fff" fontSize="40px"/>
        </a>
-       <a href='mailto:edwinzhang64@gmail.com' className='mail'>
-         <i className='icon ion-paper-airplane transition'></i>
+       <a href='mailto:edwinzhang64@gmail.com'>
+        <Ionicon className="ionicon mail" icon="md-paper-plane" color="#fff" fontSize="40px"/>
        </a>
       </div>
     );
